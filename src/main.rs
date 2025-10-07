38766acf6352c6b4cdc2b8d4c7bf4bfbc3ee4884
McFluffypootis <ziganshintimur1.5@gmail.com>
2025-10-07 08:38:24 +0200

    inital main server startup script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2production::routes;
+use zero2production::startup::*;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+
+    let port = listener.local_addr().unwrap().port();
+
+    run(listener)?.await
 }
