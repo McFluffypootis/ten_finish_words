38efb09837ec84b6a4daeb3b9965e0e06c21b755
McFluffypootis <ziganshintimur1.5@gmail.com>
2025-10-14 14:49:55 +0200

    gitignore for target file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 use std::net::TcpListener;
 
-use zero2production::routes;
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
+use tracing_log::LogTracer;
+use zero2production::configuration::get_configuration;
 use zero2production::startup::*;
+
+use tracing::{Subscriber, subscriber::set_global_default};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
+
+use zero2production::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
-    let port = listener.local_addr().unwrap().port();
+    let confguration = get_configuration().expect("Failed to read config");
 
-    run(listener)?.await
+    let connection_pool =
+        PgPool::connect(&confguration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
+
+    let address = format!("127.0.0.1:{}", confguration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind to random port");
+
+    run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,21 @@
 use std::net::TcpListener;
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
+use actix_web::{App, HttpServer, dev::Server, middleware::Logger, web};
+use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
+use tracing_log::TraceLogger;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+
+    let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
