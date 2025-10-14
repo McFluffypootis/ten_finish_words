ca69067829fe61154c6e80641520675092edfe8d
McFluffypootis <ziganshintimur1.5@gmail.com>
2025-10-14 14:36:22 +0200

    minor fix to the postgres porting

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -30,7 +30,7 @@ if [[ -z "${SKIP_DOCKER}" ]]; then
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
+    -p "${DB_PORT}" \
     -d postgres \
     postgres -N 1000
 fi
